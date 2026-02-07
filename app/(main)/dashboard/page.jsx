fc4ee6689f8aa35741c134a086e783d1133a31cb
Aadi-patel04 <aadipatel0042@gmail.com>
2026-02-08 04:41:31 +0530

    Created the budget section and budget progress bar

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -3,6 +3,8 @@ import { CreateAccountDrawer } from "@/components/create-account-drawer";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
 import { AccountCard } from "./_components/account-card";
+import { getCurrentBudget } from "@/actions/budget";
+import { BudgetProgress } from "./_components/budget-progress";
 
 
 export default async function DashboardPage() {
@@ -13,21 +15,22 @@ export default async function DashboardPage() {
 
     const accounts = await getUserAccounts();
 
-    //   const defaultAccount = accounts?.find((account) => account.isDefault);
+      const defaultAccount = accounts?.find((account) => account.isDefault);
 
     //   Get budget for default account
-    //   let budgetData = null;
-    //   if (defaultAccount) {
-    //     budgetData = await getCurrentBudget(defaultAccount.id);
-    //   }
+
+      let budgetData = null;
+      if (defaultAccount) {
+        budgetData = await getCurrentBudget(defaultAccount.id);
+      }
 
     return (
         <div className="space-y-8">
-            {/* Budget Progress
-      <BudgetProgress
+            {/* Budget Progress */}
+       <BudgetProgress
         initialBudget={budgetData?.budget}
         currentExpenses={budgetData?.currentExpenses || 0}
-      /> */}
+      />
 
       {/* Dashboard Overview */}
             {/* <DashboardOverview
